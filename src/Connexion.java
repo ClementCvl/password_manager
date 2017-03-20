cdcd986ad3c857ffda628d4caa48132b0c4c655c
Clément Cuvillier <Clment Cuvillier>
2017-03-20 23:00:26 +0100

    Debut classe connexion

--- src/Connexion.java
+++ src/Connexion.java
@@ -1,6 +1,69 @@
 /**
  * Created by Clément on 20/03/2017.
  */
-public class Connexion {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class Connexion {
+    private String DBPath = "Chemin aux base de donnée SQLite";
+    private Connection connection = null;
+    private Statement statement = null;
+
+    /**
+     *
+     * @param dBPath = chemin vers la database
+     */
+    public Connexion(String dBPath) {
+        DBPath = dBPath;
+    }
+
+    /**
+     * Connexion a la database du DBPath
+     */
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DBPath);
+            statement = connection.createStatement();
+            System.out.println("Connexion a " + DBPath + " avec succès");
+        } catch (ClassNotFoundException notFoundException) {
+            notFoundException.printStackTrace();
+            System.out.println("Erreur de connexion");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.out.println("Erreur de connexion");
+        }
+    }
+
+    /**
+     * Fermeture de la connexion
+     */
+    public void close() {
+        try {
+            connection.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Execute une requete sql
+     * @param requet
+     */
+    public ResultSet query(String requet) {
+        ResultSet resultat = null;
+        try {
+            resultat = statement.executeQuery(requet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur dans la requet : " + requet);
+        }
+        return resultat;
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,23 @@
 /**
  * Created by Clément on 20/03/2017.
  */
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Principal
 {
-    public static void main (String[] args)
-    {
-        System.out.println("Test");
+    public static void main(String[] args) {
+        Connexion connexion = new Connexion("database/Database.db");
+        connexion.connect();
+        ResultSet resultSet = connexion.query("SELECT * FROM Book WHERE Title LIKE 'Programmation JAVA' ");
+        try {
+            while (resultSet.next()) {
+                System.out.println("Titre : "+resultSet.getString("Title"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        connexion.close();
     }
 }
