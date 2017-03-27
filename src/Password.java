900ac004e674ea8846212d63d32541f3a6326158
Clement Cuvillier <cuvillier.cl@gmail.com>
2017-03-27 23:51:33 +0200

    Javadoc

--- src/Password.java
+++ src/Password.java
@@ -4,6 +4,12 @@ import java.sql.SQLException;
 /**
  * Created by Clément on 23/03/2017.
  */
+
+/**
+ * This class is used to create, update and manage password. Each password of the database equal a new object Password.
+ * @author Cuvillier Clement
+ * @version 1.0
+ */
 public class Password {
     private int id;
     private String pass;
@@ -12,6 +18,14 @@ public class Password {
     private int idUser;
     private Connexion connexion;
 
+    /**
+     * Constructor of a new password, it can be use to create a password and add it in the database or to be associated to a password in the database.
+     * @param _pass String which contain the password.
+     * @param _name Name of the password to recognize it easily.
+     * @param _note A note about the time to live of the password or some information like an associate login.
+     * @param _idUser Owner of the password.
+     * @param _connexion Instance of the database.
+     */
     public Password(String _pass,String _name, String _note, int _idUser, Connexion _connexion){
         pass = _pass;
         note = _note;
@@ -20,42 +34,81 @@ public class Password {
         connexion = _connexion;
     }
 
+    /**
+     *
+     * @return the id of a password.
+     */
     public int getId(){
         return id;
     }
 
+    /**
+     *
+     * @return the string which contain the password.
+     */
     public String getPass() {
         return pass;
     }
 
+    /**
+     *
+     * @return the note about a password.
+     */
     public String getNote() {
         return note;
     }
 
+    /**
+     *
+     * @return the name of a password.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return the owner's id of a password.
+     */
     public int getIdUser() {
         return idUser;
     }
 
+    /**
+     * Used to assign a password to the object.
+     * @param pass The new password
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
 
+    /**
+     * Used to assign a note to the object.
+     * @param note The new note.
+     */
     public void setNote(String note) {
         this.note = note;
     }
 
+    /**
+     * Used to assign a name to the object.
+     * @param name The new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Used to assign the object to an user.
+     * @param idUser The user id.
+     */
     public void setIdUser(int idUser) {
         this.idUser = idUser;
     }
 
+    /**
+     * Add a password to the database. This function use the value contains in the attribute.
+     */
     public void addPassword(){
         String query= "INSERT INTO Password(pass, note, name, idUser) VALUES(";
         query += "'" + this.getPass() +"',";
--- src/User.java
+++ src/User.java
@@ -8,43 +8,83 @@ import java.sql.SQLException;
 /**
  * Created by Clément on 23/03/2017.
  */
+
+/**
+ * This class is used to create, update and manage an user.
+ * @author Cuvillier Clement
+ * @version 1.0
+ */
 public class User {
     private int id;
     private String login;
     private String password;
     private Connexion connexion;
 
+    /**
+     * Constructor of an user.
+     * @param _login The login.
+     * @param _password For an user creation the password isn't hashed. If you create an user from the database, this password is hashed.
+     * @param _connexion Instance of the database.
+     */
     public User(String _login, String _password, Connexion _connexion){
         login = _login;
         password = _password;
         connexion = _connexion;
     }
 
+    /**
+     *
+     * @return the id of an user.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @return the login of an user.
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     *
+     * @return The password (hashed or not) of an user.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Used to assign an id to the user. This id shouldn't be assigned manually, this function is only use in the function addUser to assign the generated id to the attribute.
+     * @param _id
+     */
     public void setId(int _id) {
         id = _id;
     };
 
+    /**
+     *
+     * @param login
+     */
     public void setLogin(String login) {
         this.login = login;
     }
 
+    /**
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
 
+    /**
+     * This function is used in the addUser function to retrieve the id after the insertion.
+     * @return the id of the couple login-password
+     */
     public int selectLastId(){
         int id = 0;
         String query="SELECT id FROM User ORDER BY id DESC LIMIT 1";
@@ -56,6 +96,10 @@ public class User {
         }
         return id;
     }
+
+    /**
+     * Add an user to the database. This function use the value contains in the attribute.
+     */
     public void addUser(){
         SHA512 passhash = new SHA512(this.getPassword());
         String query = "INSERT INTO User(login, password) VALUES(";
@@ -66,6 +110,9 @@ public class User {
     }
 
 
+    /**
+     * Retrieve user information with user ID.
+     */
     public void selectUserInfo(){
         ResultSet resultSet = connexion.query("SELECT * FROM User WHERE id="+this.getId());
         try {
@@ -79,6 +126,9 @@ public class User {
         }
     }
 
+    /**
+     * Retrieve passwords created by the user.
+     */
     public void selectAllPasswordByUser(){
         ResultSet resultSet = this.connexion.query("SELECT * FROM Password WHERE idUser="+this.getId()+";");
         try {
